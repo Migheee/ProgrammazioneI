41d3f6d2fe1584cd25d564394d34bc4ae3f2b93c
admin <admin@THINKPAD-T16P.4ServiceSol.loc>
2025-03-05 10:35:08 +0100

    Revising Function, checking exercises of exam

---
+++ function/function.c
@@ -0,0 +1,22 @@
+/*
+Dato un numero, stampare la somma del numero e del suo inverso
+*/
+
+int somma(int);
+
+int main(){
+    int num;
+    scanf("%d", &num);
+    printf("%d\n", somma(num));
+    return 0;
+}
+
+int somma(int num){
+    int copia = num;
+    int inverso = 0;
+    while(copia>0){
+        inverso = inverso*10 + copia%10;
+        copia = copia/10;
+    }
+    return num+inverso;
+}
