1457ba167a1932c13b3590bf9827e96decd50628
admin <admin@THINKPAD-T16P.4ServiceSol.loc>
2025-03-05 10:16:58 +0100

    Bubble sorting algorithm

---
+++ sorting/bubble.c
@@ -0,0 +1,36 @@
+/*
+Ordinamento di array 
+BUBBLE SORTING: confronto due a due gli elementi e li scambio se non sono in ordine
+ad esempio
+[1,3,8,0]
+[1,3,0,8]
+[1,0,3,8]
+[0,1,3,8]
+*/
+
+#include <stdio.h>
+#define N 10
+int main(){
+    int array[N];
+    int tmp;
+    int i;
+    int dim;
+    int scambio;
+    for(i=0; i<N; i++){
+        scanf("%d", &array[i]);
+    }
+
+    for(int i=0; i<N-1; i++){
+        for(int j=0; j<N-1; j++){
+            if(array[j]>array[j+1]){
+                tmp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=tmp;
+            }
+        }
+    }
+    for(i=0; i<N; i++){
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
