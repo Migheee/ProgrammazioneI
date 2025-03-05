5fd733c90bd86e0d4014adac797978a55a1f82bb
admin <admin@THINKPAD-T16P.4ServiceSol.loc>
2025-03-05 10:22:19 +0100

    Bubble sorting algorithm optimization

--- sorting/bubble.c
+++ sorting/bubble.c
@@ -21,7 +21,7 @@ int main(){
     }
 
     for(int i=0; i<N-1; i++){
-        for(int j=0; j<N-1; j++){
+        for(int j=0; j<N-1-i; j++){
             if(array[j]>array[j+1]){
                 tmp=array[j];
                 array[j]=array[j+1];
