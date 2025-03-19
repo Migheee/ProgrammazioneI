6467fbe9b776191099d89291116b2e7a560baa3c
MicheleCastenedoli <mighecaste@gmail.com>
2025-03-19 10:21:54 +0100

    Switch-Case Exercise

--- switch-case/introduction.c
+++ switch-case/introduction.c
@@ -6,7 +6,7 @@ Esempio con char
 char n;
 scanf("%c", &n);
 switch(n){
-    case: '0': printf("zero");
+    case '0': printf("zero");
     case '1': printf("uno");
     case '2': printf("due");
     .....
@@ -18,7 +18,7 @@ quindi se n='2', con lo switch eseugirei tutte le istruzioni  da case '2' in poi
 char n;
 scanf("%c", &n);
 switch(n){
-    case: '0': printf("zero"); break;
+    case '0': printf("zero"); break;
                 
     case '1': printf("uno"); break;
     case '2': printf("due"); break;
@@ -27,3 +27,36 @@ switch(n){
 }
 
 */
+
+//data una sequenza che termina con '\n' stampo frequenze caratteri numerici
+
+#include <stdio.h>
+
+#define DIM 10
+
+int main(){
+    char c;
+    int a[DIM]={0};
+    int i;
+    scanf(" %c", &c);
+    while(c!='\n'){
+        switch (c)
+        {
+            case '0': a[0]++;break;
+            case '1': a[1]++;break;
+            case '2': a[2]++;break;
+            case '3': a[3]++;break;
+            case '4': a[4]++;break;
+            case '5': a[5]++;break;
+            case '6': a[6]++;break;
+            case '7': a[7]++;break;
+            case '8': a[8]++;break;
+            case '9': a[9]++;break;
+        }
+        scanf("%c", &c);
+    }
+    for(int i=0; i<DIM; i++){
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
